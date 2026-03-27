efa8b68d0ff175dc3f1d3c17d2ed622534103804
samet çopur <sametcopur@yahoo.com>
2026-03-27 23:28:04 +0300

    Update solver.py

--- marsopt/solver.py
+++ marsopt/solver.py
@@ -679,7 +679,7 @@ class Study:
             best_iteration = None
 
             if self.final_noise is None:
-                self.final_noise = min(1.0 / n_trials, self.initial_noise)
+                self.final_noise = max(1e-7, min(1.0 / n_trials, self.initial_noise))
 
             self.n_trials = n_trials
             self._objective_values = np.empty(shape=(n_trials,), dtype=np.float64)
