1ef07ba129d05f5fb695774105a9af65ccbcfb90
samet çopur <sametcopur@yahoo.com>
2026-04-09 19:16:11 +0300

    up

--- setup.py
+++ setup.py
@@ -1,39 +1,55 @@
-import numpy as np
 from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+import platform
 
-try:
-    from Cython.Build import cythonize
-    USE_CYTHON = True
-except ImportError:
-    USE_CYTHON = False
+numpy_include_dir = np.get_include()
+sysname = platform.system()
 
-ext = ".pyx" if USE_CYTHON else ".c"
+if sysname == "Windows":
+    extra_compile_args = ["/O2", "/GL", "/Gw", "/Gy", "/fp:fast", "/DNDEBUG"]
+    extra_link_args = ["/LTCG"]
+elif sysname == "Darwin":
+    extra_compile_args = ["-O3", "-flto", "-fvisibility=hidden", "-DNDEBUG"]
+    extra_link_args = ["-flto"]
+else:
+    extra_compile_args = ["-O3", "-flto", "-fvisibility=hidden", "-fPIC", "-DNDEBUG"]
+    extra_link_args = ["-flto"]
+
+define_macros = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
 
 extensions = [
     Extension(
         "marsopt.variable",
-        ["marsopt/variable" + ext],
-        include_dirs=[np.get_include()],
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        ["marsopt/variable.pyx"],
+        include_dirs=[numpy_include_dir],
+        define_macros=define_macros,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
+        language="c",
     ),
     Extension(
         "marsopt.solver",
-        ["marsopt/solver" + ext],
-        include_dirs=[np.get_include()],
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        ["marsopt/solver.pyx"],
+        include_dirs=[numpy_include_dir],
+        define_macros=define_macros,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args,
         language="c++",
     ),
 ]
 
-if USE_CYTHON:
-    extensions = cythonize(
-        extensions,
-        compiler_directives={
-            "language_level": "3",
-            "boundscheck": False,
-            "wraparound": False,
-            "cdivision": True,
-        },
-    )
+ext_modules = cythonize(
+    extensions,
+    language_level=3,
+    compiler_directives={
+        "boundscheck": False,
+        "wraparound": False,
+        "nonecheck": False,
+        "cdivision": True,
+        "infer_types": True,
+        "embedsignature": True,
+    },
+)
 
-setup(ext_modules=extensions)
+setup(ext_modules=ext_modules)
